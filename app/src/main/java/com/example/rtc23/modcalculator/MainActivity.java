61cc9e95ba530b3089c2a8ce3ca571f1a9417ba8
RTC23 <rtc23@RTC23de-iMac.local>
2018-04-09 16:39:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rtc23/modcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rtc23.modcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
